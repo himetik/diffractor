8aeb42c0599e57cd25ebd7b1aef763ef9312dc5f
George <solidpostbox@gmail.com>
2024-06-22 21:24:51 +0300

    add json format

--- gendiff/formatters/formatter.py
+++ gendiff/formatters/formatter.py
@@ -1,19 +1,23 @@
-
 """Format diff according to formatter type."""
 
 from gendiff.formatters.plain import plain
 from gendiff.formatters.stylish import stylish
+from gendiff.formatters.json import json_format
 
 
 def format_diff(diff, formatter_type):
     """
     Format diff according to formatter type.
+
     Args:
         diff (list): diff of two dictionaries
         formatter_type (str): format type for diff
+
     Returns:
         str
     """
     if formatter_type == 'plain':
         return plain(diff)
+    elif formatter_type == 'json':
+        return json_format(diff)
     return stylish(diff)
