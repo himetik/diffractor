12a220147db92cfc2b23bd3bedff8dee5bbddd45
George <solidpostbox@gmail.com>
2024-06-26 17:10:29 +0300

    fix parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,11 +1,8 @@
-"""Parse files."""
-
 import json
 import pathlib
 
 from yaml import CLoader as Loader
 from yaml import load as yaml_load
-
 
 def parse_json(json_data):
     """
@@ -19,7 +16,6 @@ def parse_json(json_data):
     """
     return json.load(json_data)
 
-
 def parse_yaml(yaml_data):
     """
     Parse yaml data.
@@ -32,6 +28,17 @@ def parse_yaml(yaml_data):
     """
     return yaml_load(yaml_data, Loader=Loader)
 
+def get_file_extension(file_path):
+    """
+    Get file extension from file path.
+
+    Args:
+        file_path (str): file path
+
+    Returns:
+        str: file extension
+    """
+    return pathlib.Path(file_path).suffix
 
 def parse_file(file_path):
     """
@@ -49,16 +56,13 @@ def parse_file(file_path):
         '.yaml': parse_yaml,
     }
 
-    file_path_obj = pathlib.Path(file_path)
-    file_abs_path = file_path_obj.resolve()
-    file_extension = file_path_obj.suffix
+    file_extension = get_file_extension(file_path)
 
-    if file_extension in parsers:
-        with open(file_abs_path) as file_obj:
-            return parsers[file_extension](file_obj)
-    else:
+    if file_extension not in parsers:
         raise ValueError(
             f'Unsupported file type: "{file_extension}". '
             f'Supported file types: {", ".join(parsers.keys())}'
         )
 
+    with open(file_path) as file_obj:
+        return parsers[file_extension](file_obj)
