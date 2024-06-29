f025771c9910ff64d2042ded792ecbf1fbb83dcc
George <solidpostbox@gmail.com>
2024-06-29 18:40:08 +0300

    gendiff exception fix

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -49,7 +49,7 @@ def generate_diff(file1_path, file2_path, formatter_type='stylish'):
         dict1 = parse_file(file1_path)
         dict2 = parse_file(file2_path)
     except ValueError as err:
-        return str(err)
+        raise ValueError form err
 
     diff = compare_dicts(dict1, dict2)
 
