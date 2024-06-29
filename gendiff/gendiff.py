e27c8c6ba7f84d1120625296fe4e84f3c8b75665
George <solidpostbox@gmail.com>
2024-06-29 19:14:20 +0300

    ---

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -33,17 +33,13 @@ def format_diff(diff, formatter_type):
 
 def generate_diff(file1_path, file2_path, formatter_type='stylish'):
     """
+
     Generate diff comparing two json, yaml files.
 
     Args:
-
         file1_path (str): path to first file
         file2_path (str): path to second file
         formatter_type (str): the format in which the diff will be formatted
-
-
-    Raises:
-        ValueError: If formatter_type is invalid.
 
 
     Returns:
@@ -53,9 +49,8 @@ def generate_diff(file1_path, file2_path, formatter_type='stylish'):
         dict1 = parse_file(file1_path)
         dict2 = parse_file(file2_path)
     except ValueError as err:
-        raise ValueError(str(err))
+        return str(err)
 
     diff = compare_dicts(dict1, dict2)
 
     return format_diff(diff, formatter_type)
-    
