a78aece19310e22882d524bdf9ee49f4fdcf97d5
ducker <himetik@tutamail.com>
2024-08-02 13:18:47 +0300

    come-back-commit

--- Makefile
+++ Makefile
@@ -25,6 +25,6 @@ publish:
 	poetry publish --dry-run
 
 package-install:
-	python3 -m pip install --user dist/*.whl
+	python3 -m pip install dist/*.whl
 
 .PHONY: install test lint selfcheck check build publish package-install
--- gendiff/compare.py
+++ gendiff/compare.py
@@ -1,6 +1,28 @@
 """Compare two dicts."""
 
 
+def get_dict_value(dictionary, key):
+    """
+    Get value from dictionary by key.
+
+    Args:
+        dictionary (dict): json.read result
+        key: dict key
+
+    Returns:
+        dict value by key
+    """
+    if key not in dictionary:
+        return None
+
+    dict_val = dictionary[key]
+    if isinstance(dict_val, bool):
+        return str(dict_val).lower()
+    if dict_val is None:
+        return 'null'
+    return dict_val
+
+
 def compare_dicts(dict1, dict2):
     """
     Compare two dicts.
@@ -16,21 +38,24 @@ def compare_dicts(dict1, dict2):
 
     diff = []
     for key in sorted(keys):
+        value1 = get_dict_value(dict1, key)
+        value2 = get_dict_value(dict2, key)
+
         if key not in dict1:
-            diff.append({'key': key, 'type': 'added', 'value': dict2[key]})
+            diff.append({'key': key, 'type': 'added', 'value': value2})
         elif key not in dict2:
-            diff.append({'key': key, 'type': 'removed', 'value': dict1[key]})
-        elif isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
-            children = compare_dicts(dict1[key], dict2[key])
+            diff.append({'key': key, 'type': 'removed', 'value': value1})
+        elif isinstance(value1, dict) and isinstance(value2, dict):
+            children = compare_dicts(value1, value2)
             diff.append({'key': key, 'type': 'complex', 'children': children})
-        elif dict1[key] != dict2[key]:
+        elif value1 != value2:
             diff.append({
                 'key': key,
                 'type': 'changed',
-                'old-value': dict1[key],
-                'new-value': dict2[key],
+                'old-value': value1,
+                'new-value': value2,
             })
         else:
-            diff.append({'key': key, 'type': 'same', 'value': dict1[key]})
+            diff.append({'key': key, 'type': 'same', 'value': value1})
 
     return diff
